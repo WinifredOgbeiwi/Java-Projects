dc26684c3153267f8170aed6f0cede8414823bfb
winifredogbeiwi <imadeogbeiwi@gmail.com>
2025-05-09 21:58:50 +0200

    debugging test

--- src/main/java/Java_Starter/Main.java
+++ src/main/java/Java_Starter/Main.java
@@ -3,5 +3,12 @@ package Java_Starter;
 public class Main {
     public static void main(String[] arg){
         System.out.println("Hello Vistula");
+
+        //Testing debugging features
+        try{
+            System.out.println(3/0);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
